980d68bd6c0d73cafcf2639414c6f6a259001eb1
aka <aka@aka.com>
2021-10-10 19:10:55 +0300

    first

---
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @RequestMapping("/")
+    public String welcome(){
+        return "Hello from Maven web!!!";
+
+    }
+}
